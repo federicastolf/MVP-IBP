f3a7b89b90498f06103a040dc6df29d45a5fc1e4
federicastolf <federica.stolf@gmail.com>
2024-02-13 17:23:04 -0500

    .

---
+++ accumulationCurve.R
@@ -0,0 +1,196 @@
+
+library(Rcpp) 
+library(RcppArmadillo)
+library(tidyverse)
+library(latex2exp)
+library(gridExtra)
+library(bigMVP)
+
+rm(list=ls())
+source("rfuncts/MVPIBP_functions.R")
+Rcpp::sourceCpp("rcppfuncts/MVPIBPh.cpp")
+
+#-----# load data #-----#
+load("Data/X_fungi.Rdata")
+load("data/fungi_binary.Rdata")
+cov_data = as.data.frame(X_fungi)
+Nrep = 50
+
+#--------# observed pstar #---------#
+
+## site 1 and 2
+fungi1 = fungi[cov_data$site_id1 == 1,]
+cov1 = cov_data[cov_data$site_id1 == 1,]
+nf1 = nrow(fungi1)
+fungi2 = fungi[cov_data$site_id1 == 0 & cov_data$site_id3 == 0  & 
+                 cov_data$site_id4 == 0,]
+cov2 = cov_data[cov_data$site_id1 == 0 & cov_data$site_id3 == 0  & 
+                  cov_data$site_id4 == 0,]
+nf2 = nrow(fungi2)
+pstar1_obs = compute_pstar(fungi1)
+pstar2_obs = compute_pstar(fungi2)
+
+#---------------# test and train set #-------------------#
+
+set.seed(854)
+# site 1
+Nobs_test1 = round(nf1*0.25)
+sampled_rows1 = sample(nf1, Nobs_test1)
+row_indices1 = which(cov_data$site_id1 == 1)[sampled_rows1]
+X_train1 = X_fungi[-row_indices1,]
+Y_train1 = fungi[-row_indices1, ]
+# site2
+Nobs_test2 = round(nf2*0.25)
+sampled_rows2 = sample(nf2, Nobs_test2)
+row_indices2 = which(cov_data$site_id1 == 0 & cov_data$site_id3 == 0
+                     & cov_data$site_id4 == 0)[sampled_rows2]
+X_train2 = X_fungi[-row_indices2,]
+Y_train2 = fungi[-row_indices2, ]
+
+##########################################################################
+####----------------#### out of sample prediction #####--------------#####
+##########################################################################
+
+####--------------------------#### fit bigMVPh ####-------------------------####
+
+# NB in bigMVP the input model matrix X doesn't include the intercept
+q1 = ncol(X_train1) -1
+q2 = ncol(X_train2)-1
+eta01 = rep(0, q1+1)
+eta02 = rep(0, q2+1)
+nu0 = 0.001
+gamma01 = q1+1
+gamma02 = q2+1
+Lambda01 = diag(q1+1)
+Lambda02 = diag(q1+1)
+eta0_rho = 0
+nu0_rho = 0.01
+gamma0_rho = 0.01
+lambda0_sq_rho = 0.01
+max_it = 100
+epsilon = 0.0001
+m = 15
+nmcmc = 200
+burnin = 50
+truncP = 200
+Y1b = cbind(Y_train1, matrix(0, nrow(Y_train1), truncP))
+Y2b= cbind(Y_train2, matrix(0, nrow(Y_train2), truncP))
+set.seed(434)
+bigMVPfit1 = bigMVPh(Y1b, X_train1[,-1], eta01, nu0, gamma01, Lambda01, 
+                     eta0_rho, nu0_rho, gamma0_rho, lambda0_sq_rho, max_it, 
+                     epsilon, m, nmcmc, burnin)
+bigMVPfit2 = bigMVPh(Y2b, X_train2[,-1], eta02, nu0, gamma02, Lambda02, 
+                     eta0_rho, nu0_rho, gamma0_rho, lambda0_sq_rho, max_it, 
+                     epsilon, m, nmcmc, burnin)
+
+
+#------# predictions for p*_n #-------#
+mseed = 354
+# site 1
+pstar1_bM = pred_pstarMVP(bigMVPfit1, Nrep, mseed, cov1, fungi1)
+pstar1q_bM = apply(pstar1_bM, 2, quantile, probs=c(0.05,0.95))
+pstar1m_bM = apply(pstar1_bM, 2, mean)
+# site 2
+pstar2_bM = pred_pstarMVP(bigMVPfit2, Nrep, mseed, cov2, fungi2)
+pstar2q_bM = apply(pstar2_bM, 2, quantile, probs=c(0.05,0.95))
+pstar2m_bM = apply(pstar2_bM, 2, mean)
+
+
+###-----------------### fit hierarchical MVP-IBP model ####------------------###
+a_alpha = 17.5
+b_alpha = 0.25
+eps_MH = 0.2
+prior_var = 15
+
+mvpIBPfit1 = MVP_IBPh(Y_train1, X_train1, eta0_rho, nu0_rho, gamma0_rho, lambda0_sq_rho, 
+                      a_alpha, b_alpha,prior_var,max_it, epsilon, m, nmcmc, burnin, 
+                      eps_MH, truncP)
+mvpIBPfit2 = MVP_IBPh(Y_train2, X_train2, eta0_rho, nu0_rho, gamma0_rho, lambda0_sq_rho, 
+                      a_alpha, b_alpha,prior_var,max_it, epsilon, m, nmcmc, burnin, 
+                      eps_MH, truncP)  
+
+#------# predictions for p*_n #-------#
+
+# site 1
+pstar1_MVPIBP = pred_pstarMVP(mvpIBPfit1, Nrep, mseed, cov1, fungi1)
+pstar1o_q = apply(pstar1_MVPIBP, 2, quantile, probs=c(0.05,0.95))
+pstar1o_m = apply(pstar1_MVPIBP, 2, mean)
+
+# site 2
+pstar2_MVPIBP = pred_pstarMVP(mvpIBPfit2, Nrep, mseed, cov2, fungi2)
+pstar2o_q = apply(pstar2_MVPIBP, 2, quantile, probs=c(0.05,0.95))
+pstar2o_m = apply(pstar2_MVPIBP, 2, mean)
+
+
+###-------------------------###  IBP ####------------------------------####
+
+#---------# fit IBP #-------#
+Y_train1IBP = cbind(fungi1[-sampled_rows1, ])
+Y_train2IBP = cbind(fungi2[-sampled_rows2, ])
+MCMC_IBP = 2000
+burnin_IBP = 1000
+param_IBP = list(truncP = truncP, a_alpha = a_alpha, b_alpha = b_alpha)
+IBP1 = Beta_Binom_IBP(Y_train1IBP, param_IBP, MCMC_IBP, mseed)
+IBP2 = Beta_Binom_IBP(Y_train2IBP, param_IBP, MCMC_IBP, mseed)
+
+#-------# predictions #-------#
+pstar1_IBP = pred_pstarIBP(IBP1, Nrep, mseed, burnin_IBP, MCMC_IBP, fungi1)
+pstar1IBP_q = apply(pstar1_IBP, 2, quantile, probs=c(0.05,0.95))
+pstar1IBP_m = apply(pstar1_IBP, 2, mean)
+
+## site 2
+pstar2_IBP = pred_pstarIBP(IBP2, Nrep, mseed, burnin_IBP, MCMC_IBP, fungi2)
+pstar2IBP_q = apply(pstar2_IBP, 2, quantile, probs=c(0.05,0.95))
+pstar2IBP_m = apply(pstar2_IBP, 2, mean)
+
+
+###################################################
+####-------------##### PLOT ####-------------##### 
+
+
+data_site1 = cbind.data.frame(c(1:nf1),  t(pstar1o_q), pstar1o_m, t(pstar1IBP_q),
+                              pstar1IBP_m, t(pstar1q_bM), pstar1m_bM, pstar1_obs, 
+                              rep(nf1 - Nobs_test1, nf1), rep("Site1", nf1))
+colnames(data_site1) = c("n", "lb_MV", "ub_MV", "median_MV", "lb_IB", "ub_IB",
+                         "median_IB", "lb_bM", "ub_bM", "median_bM","obs",
+                         "test_tresh","site")
+data_site2 = cbind.data.frame(c(1:nf2),  t(pstar2o_q), pstar2o_m, t(pstar2IBP_q),
+                              pstar2IBP_m, t(pstar2q_bM), pstar2m_bM, pstar2_obs, 
+                              rep(nf2 - Nobs_test2, nf2), rep("Site2", nf2))
+colnames(data_site2) = c("n", "lb_MV", "ub_MV", "median_MV", "lb_IB", "ub_IB",
+                         "median_IB","lb_bM", "ub_bM", "median_bM", "obs", 
+                         "test_tresh","site")
+dataplot = rbind.data.frame(data_site1, data_site2)
+
+shadealpha = 0.4
+linesize = 1
+szp = 2
+stp = 1
+f1 = ggplot(data = dataplot) +
+  geom_ribbon(aes(x = n, ymax = ub_IB, ymin = lb_IB), alpha = shadealpha,  fill = "steelblue") +
+  geom_line(aes(n, ub_IB), linetype= "dotted", color = "steelblue") +
+  geom_line(aes(n, lb_IB), linetype = "dotted", color = "steelblue") +
+  geom_line(aes(n, median_IB, color = "forestgreen"), linewidth = linesize) +
+  geom_ribbon(aes(x = n, ymax = ub_bM, ymin = lb_bM), alpha = shadealpha, fill = "forestgreen") +
+  geom_line(aes(n, lb_bM), linetype= "dotted", color = "forestgreen") +
+  geom_line(aes(n, ub_bM), linetype = "dotted", color = "forestgreen") +
+  geom_line(aes(n, median_bM, color = "steelblue"), linewidth = linesize) +
+  geom_ribbon(aes(x = n, ymax = ub_MV, ymin = lb_MV), alpha = shadealpha, fill = "red") +
+  geom_line(aes(n, lb_MV), linetype= "dotted", color = "red") +
+  geom_line(aes(n, ub_MV), linetype = "dotted", color = "red") +
+  geom_line(aes(n, median_MV, color = "darkred"), linewidth = linesize) +
+  geom_point(aes(n, obs), color = 'black', shape = 19,
+             size = stp, stroke = stp, alpha=0.5) +
+  scale_color_manual(values=c("darkred","steelblue", "forestgreen"),
+                     labels = c("MVP-IBP", "IBP", "bigMVP")) +
+  geom_vline(aes(xintercept = test_tresh), linetype= "longdash", linewidth = 0.8) +
+  labs(y = TeX("$p^*_n$"), x= "n") +
+  theme_light() + 
+  facet_wrap(~ site, scales = "free") +
+  theme(text = element_text(size = 16), legend.title=element_blank(),
+        legend.text = element_text(size=16), 
+        legend.position = "top", strip.text = element_text(size=16))
+
+f1
+
+# ggsave(filename = "pstar_fungi1.png", plot=f1,  width = 12, height = 7)
---
+++ rcppfuncts/MVPIBP_functions.R
@@ -0,0 +1,269 @@
+require(Matrix)
+require(mvnfast)
+
+######################## functions GIBBS MVP-IBP #######################
+########################################################################
+
+inverse_SHM = function(M, c = 1){
+  "----------------------------------------------------------------------------
+  compute efficiently the inverse of a matrix with structure MM^T + cI 
+  exploiting the Sherman–Morrison–Woodbury formula
+  ----------------------------------------------------------------------------"
+  psi0 = diag(1/c, NROW(M))
+  for (i in 1:NCOL(M)){
+    lambdak = M[,i]
+    wk = eigenMapMatMult(psi0, lambdak)
+    psi_k = psi0 - (eigenMapMatMult(wk, t(wk)))/(1+ sum(lambdak*wk))
+    psi0 = psi_k
+  }
+  return(psi_k)
+}
+
+compute_invR = function(Lambda){
+  sd_psi = sqrt(rowSums(Lambda^2) + 1) 
+  inv_psi = inverse_SHM(Lambda)
+  invR = t(t(inv_psi*sd_psi)*sd_psi)
+  invR
+}
+
+# Log likelihood for alpha for MH
+log_lik_alpha = function(alpha, beta){
+  p = length(beta)
+  tau2 = 2*log(p)
+  mu = sqrt(1 + tau2)*qnorm(alpha/(alpha + p))
+  if (alpha > 0) {
+    logl = dnorm(beta, mu, sqrt(tau2), log = TRUE)
+    return(sum(logl))
+  } else {
+    return(-Inf)
+  }
+}
+
+# Log-posterior for MH - log parametrization
+logpost_alpha_log = function(alpha, beta, a_alpha, b_alpha){
+  log_likelihood = log_lik_alpha(alpha, beta)
+  return(log_likelihood + dgamma(exp(alpha), a_alpha, b_alpha, log = TRUE) + alpha)
+}
+
+# Log-posterior for MH 
+logpost_alpha = function(alpha, beta, a_alpha, b_alpha){
+  log_likelihood = log_lik_alpha(alpha, beta)
+  return(log_likelihood + dgamma(alpha, a_alpha, b_alpha, log = TRUE))
+}
+
+################################ simulation #################################
+#############################################################################
+
+synthetic_data = function(n, p, alpha, my_seed, type, k_factor = 10){
+  "----------------------------------------------------------------------------
+  simulate binary data matrix in different scenarios: 
+  - factor (MVP-IBP factor model)
+  - common (MVP-IBP where Sigma has an equi-correlation structure)
+  - ts (MVP-IBP where the latent variables follow a t-student distribution)
+  ----------------------------------------------------------------------------"
+  set.seed(my_seed)
+  b_sd = sqrt(2*log(p))
+  b_mu = sqrt(1+b_sd^2)*qnorm(alpha/(alpha + p))
+  beta = rnorm(p, b_mu, b_sd)
+  data_MVPIBP = matrix(0, nrow = n, ncol = p)
+  z = rep(NA, p)
+
+  
+  # factor 
+  if(type=="factor"){
+    Lambda = matrix(rnorm(p*k_factor, mean = 1), p, k_factor)
+    beta_tilde = beta*sqrt(rowSums(Lambda^2) +1)
+    for (i in 1:n){
+      eps = rnorm(p)
+      eta = rnorm(k_factor)
+      z = beta_tilde + Lambda %*% eta + eps
+      data_MVPIBP[i,] = as.numeric(z>0)
+    }
+    sq_D = sqrt(rowSums(Lambda^2) + 1)
+    Sigma =  diag(1/sq_D) %*% (Lambda %*% t(Lambda) + diag(p)) %*% diag(1/sq_D)
+    beta = beta_tilde
+  }
+
+  #common
+  if(type=="common"){
+    rhot = runif(1, 0, 0.8)
+    Sigma = matrix(rhot, p, p)
+    diag(Sigma) = 1
+    for(i in 1:n){
+      eps = mvnfast::rmvn(1, mu=rep(0,p), sigma = Sigma)
+      z = beta + eps
+      data_MVPIBP[i,] = as.numeric(z>0)
+    }
+  }
+  
+  # t-student
+  if(type=="ts"){
+    Lambda = matrix(rnorm(p*k_factor), p, k_factor)
+    beta_tilde = beta*sqrt(rowSums(Lambda^2) +1)
+    for (i in 1:n){
+      eps = rt(p, df = 10)
+      eta = rnorm(k_factor)
+      z = beta_tilde + Lambda %*% eta + eps
+      data_MVPIBP[i,] = as.numeric(z>0)
+    }
+    sq_D = sqrt(rowSums(Lambda^2) + 1)
+    Sigma =  diag(1/sq_D) %*% (Lambda %*% t(Lambda) + diag(p)) %*% diag(1/sq_D)
+    beta = beta_tilde
+  }
+  active = apply(data_MVPIBP != 0, 2, any)
+  pstar = sum(active)
+  data_active = data_MVPIBP[,active]
+  pi = pnorm(beta)
+  pi_active =  pnorm(beta)[active]
+  Sigma_active = Sigma[active, active]
+  return(list("data_all" = data_MVPIBP, "Sigma" = Sigma, "beta" = beta, 
+              "pi" = pi, "Sigma_active" = Sigma_active, "pi_active" = pi_active,
+              "active"= active, "pstar" = pstar, "data" = data_active,
+              "type" = type))
+}
+
+simulation_data = function(alpha, n, p, type, my_seed){
+  Ndata = length(alpha)*length(type)
+  set.seed(my_seed)
+  seeds_D = sample.int(5000, Ndata)
+  dataALLsynth = vector("list", Ndata)
+  for(i in 1:length(type)){
+    for(k in 1:length(alpha)){
+      dataALLsynth[[(k-1)*length(type) + i]] = synthetic_data(n, p, alpha[k],
+                                                 seeds_D[(k-1)*length(type) + i],
+                                                type[i])
+    }
+  }
+  dataALLsynth
+}
+
+
+############################ Gibbs for IBP ################################
+###########################################################################
+
+
+Beta_Binom_IBP = function(data, param, Niter, my_seed){
+  "----------------------------------------------------------------------------
+  Gibbs for truncated Beta-Binomial rapresentation of the IBP
+  ----------------------------------------------------------------------------"
+  set.seed(my_seed)
+  n = dim(data)[1]
+  data = cbind(data, matrix(0, n, param$truncP))
+  p = dim(data)[2]
+  m_j = colSums(data) # occurence for each species
+  # posterior draws of the logarithmic occurrence probabilities within each habitats
+  PROB = matrix(0, p, Niter)  
+  alpha = rep(0, Niter) # posterior draws of the alpha parameters
+  # initialize alpha
+  alpha[1] = rgamma(1, shape = param$a_alpha, rate = param$b_alpha)  
+  # Gibbs sampling steps
+  for (iter in 2:Niter){
+    # full conditional for probabilities
+    for (j in 1:p){
+      PROB[j, iter] = log(rbeta(1, alpha[iter-1]/p + m_j[j], n - m_j[j] + 1))
+    }
+    # full conditional for alphas
+    alpha[iter] = rgamma(1, param$a_alpha, param$b_alpha - sum(PROB[iter])/p)
+  }
+  expP = exp(PROB)
+  return(list("prob" = expP, "alpha" = alpha))
+}
+
+
+############################ plot ############################################
+###############################################################################
+
+build_phylo = function(covariate, fit_MVPIBP, tax_matrix, Xmodel){
+  "----------------------------------------------------------------------------
+  Return a phyloseq object to make Krona wheels plot
+  - covariate: covariate from w
+  - fit_MVP_IBP: output from a hierarchical MVP-IBP model
+  - tax_matrix: data frame with taxonoical tree classification of data 
+  - Xmodel: model matrix covariate used for fitting fit_MVPIBP
+  ----------------------------------------------------------------------------"
+  
+  p = ncol(fit_MVPIBP$Y)
+  n = nrow(fit_MVPIBP$Y)
+  
+  # posterior mean of marginal species occurence
+  pi = pnorm(Xmodel %*% fit_MVPIBP$coefficients)[,1:p]
+  
+  # otu_matrix
+  otu_mat = as.data.frame(t(pi))
+  rownames(otu_mat) = paste0("otu", 1:p)
+  colnames(otu_mat) = paste0("s", 1:n)
+  otu_mat = as.matrix(otu_mat)
+  
+  # construct samples_df
+  covariate_fungi = cbind.data.frame(c(1:n), covariate_fungi)
+  colnames(covariate_fungi)[1] = "sample"
+  covariate_fungi = cbind.data.frame(covariate_fungi, rep("a",NROW(covariate_fungi)))
+  colnames(covariate_fungi)[4] = "no_cov"
+  samples_df = covariate_fungi %>% 
+    tibble::column_to_rownames("sample") 
+  rownames(samples_df) = paste0("s", 1:n)
+  colnames(samples_df)[1]="site_id"
+  
+  # obtain phyloseq object
+  OTU = otu_table(otu_mat, taxa_are_rows = TRUE) 
+  samples = sample_data(samples_df)
+  TAX = tax_table(tax_matrix)
+  fungi_phylo = phyloseq(OTU, TAX, samples)
+  return(fungi_phylo)
+}
+
+
+compute_pstar = function(data){
+  # compute pstar for all rows of data
+  n = nrow(data)
+  pstar = rep(0, n)
+  pstar[1] = sum(data[1,])
+  for(i in 2:n){
+    datai = data[c(1:i),]
+    pstar[i] = sum(apply(datai != 0, 2, any))
+  }
+  pstar
+}
+
+
+pred_pstarMVP = function(fit_MVPIBP, Nrep, seed, covariate, Y){
+  nf = nrow(covariate)
+  X = as.matrix(covariate)
+  p = ncol(Y)
+  beta_est = fit_MVPIBP$coefficients[,1:p]
+  q = nrow(beta_est)
+  pstar_out = matrix(0, Nrep, nf)
+  set.seed(seed)
+  for(n in 1:Nrep){
+    beta_sample = matrix(NA, q, NCOL(beta_est))
+    for(j in 1:NCOL(beta_est)){
+      sigma_est = matrix(fit_MVPIBP$cov_mats[,j], q, q)
+      muj = beta_est[,j]
+      beta_sample[,j] = mvnfast::rmvn(1, muj, sigma_est)
+    }
+    pi_hat = pnorm(X %*% beta_sample)
+    pi_mean = apply(pi_hat,2,mean)
+    datasim1 = matrix(0, nrow(pi_hat), length(pi_mean))
+    for(i in 1:nrow(pi_hat)){
+      datasim1[i,]=rbinom(length(pi_mean), 1, pi_mean)
+    }
+    pstar_out[n,] = compute_pstar(datasim1)
+  }
+  pstar_out
+}
+
+
+pred_pstarIBP = function(IBP_fit, Nrep, seed, burnin, MCMC, data){
+  set.seed(seed)
+  p = ncol(data)
+  pi_hat = apply(IBP_fit[[1]][,burnin:MCMC],1,mean)[1:p]
+  pstar_IBP = matrix(0, Nrep, nrow(data))
+  for(n in 1:Nrep){
+    datasim1 = matrix(0,nrow(data), length(pi_hat))
+    for(i in 1:nrow(data)){
+      datasim1[i,]=rbinom(length(pi_hat),1,pi_hat)
+    }
+    pstar_IBP[n,] = compute_pstar(datasim1)
+  }
+  pstar_IBP
+}
